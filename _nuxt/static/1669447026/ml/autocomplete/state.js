ccb59a08c0973d6ececa60486c0e518967890a4b
snlangsuan <snlangsuan@users.noreply.github.com>
2022-11-26 07:17:40 +0000

    deploy: 553bcc190b8d8d6d11ae9ee392a46bcc8d93c5a2

---
+++ _nuxt/static/1669447026/ml/autocomplete/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-interesting-nuxt\u002F_nuxt\u002Fstatic\u002F1669447026",layout:"ml",error:a,serverRendered:true,routePath:"\u002Fml\u002Fautocomplete",config:{_app:{basePath:"\u002Fmy-interesting-nuxt\u002F",assetsPath:"\u002Fmy-interesting-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
