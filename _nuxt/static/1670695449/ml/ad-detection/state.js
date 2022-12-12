d4843e7ec60e73790651f14f8ccc05097bc72b36
snlangsuan <snlangsuan@users.noreply.github.com>
2022-12-12 17:30:08 +0000

    deploy: 7afa774b0badef2f2292a70b40ccc6ac6f63afb7

--- _nuxt/static/1670695449/ml/ad-detection/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-interesting-nuxt\u002F_nuxt\u002Fstatic\u002F1670695449",layout:"ml",error:a,serverRendered:true,routePath:"\u002Fml\u002Fad-detection",config:{_app:{basePath:"\u002Fmy-interesting-nuxt\u002F",assetsPath:"\u002Fmy-interesting-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
